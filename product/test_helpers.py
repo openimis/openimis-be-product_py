5d13011755ad060111824dbe2d73e124556788c4
Eric Darchis <darchis@pobox.com>
2020-02-17 12:01:49 +0100

    OMT-51 Product -> Location + dispatch test helpers
    Add some constants, a link from Product to Location and the test helpers that were extracted from the claim module

---
+++ product/test_helpers.py
@@ -0,0 +1,49 @@
+from product.models import Product, ProductService, ProductItem
+
+
+def create_test_product(code, valid=True, custom_props=None):
+    return Product.objects.create(
+        **{
+            "code": code,
+            "name": "Test product " + code,
+            "lump_sum": 123.45,
+            "member_count": 1,
+            "grace_period": 1,
+            "date_from": "2019-06-01",
+            "date_to": "2049-06-01",
+            "validity_from": "2019-06-01",
+            "validity_to": None if valid else "2019-06-01",
+            "audit_user_id": -1,
+            **(custom_props if custom_props else {})
+        }
+    )
+
+
+def create_test_product_service(product, service, valid=True, custom_props=None):
+    return ProductService.objects.create(
+        **{
+            "product": product,
+            "service": service,
+            "limitation_type": ProductService.LIMIT_CO_INSURANCE,
+            "price_origin": ProductService.ORIGIN_PRICELIST,
+            "validity_from": "2019-06-01",
+            "validity_to": None if valid else "2019-06-01",
+            "audit_user_id": -1,
+            **(custom_props if custom_props else {})
+        }
+    )
+
+
+def create_test_product_item(product, item, valid=True, custom_props=None):
+    return ProductService.objects.create(
+        **{
+            "product": product,
+            "item": item,
+            "limitation_type": ProductItem.LIMIT_CO_INSURANCE,
+            "price_origin": ProductItem.ORIGIN_PRICELIST,
+            "validity_from": "2019-06-01",
+            "validity_to": None if valid else "2019-06-01",
+            "audit_user_id": -1,
+            **(custom_props if custom_props else {})
+        }
+    )
