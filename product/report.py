6b044cc3ebad81ac40daf299519429e634b621fb
Thibault Dethier <84660492+tdethier@users.noreply.github.com>
2023-09-12 14:52:11 +0200

    OMT-331 - Product Derived Operational Indicators report (#42)
    * OMT-331 - Product Derived Operational Indicators report - WIP
    
    * WIP
    
    * Fixed report

--- product/report.py
+++ product/report.py
@@ -1,4 +1,5 @@
-from product.reports import product_sales
+from product.reports import product_sales, product_derived_operational_indicators
+from product.reports.product_derived_operational_indicators import product_derived_operational_indicators_query
 
 report_definitions = [
     {
@@ -9,5 +10,14 @@ report_definitions = [
         "module": "product",
         "python_query": product_sales.product_sales_query,
         "permission": ["131205"],
+    },
+    {
+        "name": "product_derived_operational_indicators",
+        "engine": 0,
+        "default_report": product_derived_operational_indicators.template,
+        "description": "Product Derived operational indicators",
+        "module": "product",
+        "python_query": product_derived_operational_indicators_query,
+        "permission": ["131203"],
     },
 ]
